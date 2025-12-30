2b0117156acfae1408ff35532d89b01bc9f37298
Maria Gilca <maria.gilca@openlm.com>
2025-12-30 22:12:52 +0200

    Deploy website - based on a099b301e79a258ae222a845e7e9036cac628adf

---
+++ assets/js/5e95c892.bbf2675b.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkvoix=globalThis.webpackChunkvoix||[]).push([[9647],{7121(e,s,r){r.r(s),r.d(s,{default:()=>h});r(6540);var a=r(4164),u=r(7559),c=r(5500),i=r(2831),l=r(1656),o=r(4848);function h(e){return(0,o.jsx)(c.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,o.jsx)(l.A,{children:(0,i.v)(e.route.routes)})})}}}]);
