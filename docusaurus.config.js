a099b301e79a258ae222a845e7e9036cac628adf
Maria Gilca <maria.gilca@openlm.com>
2025-12-30 22:10:05 +0200

    add correct domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,16 +19,15 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+
+  // GitHub Pages / production URL
+  url: 'https://voix.md',
   baseUrl: '/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mariagilca', // your real GitHub username
+  projectName: 'voix',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 
